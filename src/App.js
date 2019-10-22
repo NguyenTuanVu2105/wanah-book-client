db185735b2e1c84bfcf75bb27f87debf03ffe90e
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2019-10-23 01:54:31 +0700

    Third Init

--- src/App.js
+++ src/App.js
@@ -4,15 +4,5 @@ import RegisterContainer from './app/register/components/RegisterContainer'
 
 import "./App.css"
 
-class App extends Component{
-    render() {
-        return (
-            <div>
-                <LoginContainer></LoginContainer>               
-                {/* <RegisterContainer></RegisterContainer> */}
-            </div>
-        )
-    }
-}
 
 export default App;
