357ea6677e79b7d01a010f2bec36d89ecd346886
honghanh3123 <hanhanhhueduy@gmail.com>
2019-10-23 19:16:10 +0700

    edit file user.js

--- src/data/user.js
+++ src/data/user.js
@@ -1,4 +1,26 @@
-export default user = {
+export default user = [
+    {
     name: "VU",
     address: "ahihi"
-}
+    },
+    {
+        name: "VU",
+        address: "ahihi"
+    },
+    {
+        name: "VU",
+        address: "ahihi"
+    },
+    {
+        name: "VU",
+        address: "ahihi"
+    },
+    {
+        name: "VU",
+        address: "ahihi"
+    },
+    {
+        name: "VU",
+        address: "ahihi"
+    }
+]
