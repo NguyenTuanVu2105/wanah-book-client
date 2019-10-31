67d4d30e9d0da25f2a3d89938b3d26dd1ba0be2c
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2019-11-01 01:22:19 +0700

    add api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import LoginContainer from './app/login/components/LoginContainer'
 import "./App.css"
-// import {test} from "./api/base/test"
+import {test} from "./api/base/test"
 
 const App = () => {
-    // const tester = async () => {
-    //     const {data} = await test();
-    //     console.log(data)   
-    // }
-    // tester()
+    const tester = async () => {
+        const {data} = await test();
+        console.log(data)   
+    }
+    tester()
     return (
         <LoginContainer></LoginContainer> 
     )
