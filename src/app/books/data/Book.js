8c3eb1f2fa0334dd324656a0efea526476a5f061
honghanh3123 <hanhanhhueduy@gmail.com>
2019-11-12 10:04:18 +0700

    add book page

---
+++ src/app/books/data/Book.js
@@ -0,0 +1,62 @@
+export const books = [
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 5
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 3
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 4
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 3
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 5
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 1
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 5
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 2
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 1.5
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 5
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 3
+  },
+  {
+    name: "Tôi thấy hoa vàng trên cỏ xanh",
+    image: "https://sudospaces.com/chanhtuoi-com/uploads/2014/09/toi_thay_hoa_vang-x240.jpg",
+    star: 4
+  }
+]
