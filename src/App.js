a902d7b5d70c1cef6499bf661e3f8f3d66235ceb
cafrotos <hotanhiu.99@gmail.com>
2019-11-20 16:54:53 +0700

    refactor container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react'
+import { Layout, Col, Row } from 'antd';
+import { renderRoutes } from 'react-router-config'
 import './App.scss'
 import AppHeader from './app/common/header/AppHeader'
 import AppSider from './app/common/sidebar/AppSider';
 import AppContent from './app/common/content/AppContent'
 // import Content from './common/Content'
-import {Row, Container} from 'react-bootstrap';
+// import { Row, Container } from 'react-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fab, fas)
 
+const { Header, Content } = Layout
+
 const App = (props) => {
     return (
-        <Container className="homepage">
-            <AppHeader></AppHeader>
-            <Row className="app-body">
-                <AppSider></AppSider>
-                <AppContent route={props.route}></AppContent>
-            </Row>
-        </Container>  
+        // <Container className="homepage">
+        //     <AppHeader></AppHeader>
+        //     <Row className="app-body">
+        //         <AppSider></AppSider>
+        //         <AppContent route={props.route}></AppContent>
+        //     </Row>
+        // </Container>  
+        <Layout>
+            <AppHeader />
+            <Content style={{ padding: '0 50px', marginTop: 79 }}>
+                <Row type="flex">
+                    {
+                        true ?
+                            <AppSider /> :
+                            null
+                    }
+                    <AppContent route={props.route}></AppContent>
+                </Row>
+            </Content>
+        </Layout>
     )
 }
 
--- src/app/common/header/AppHeader.js
+++ src/app/common/header/AppHeader.js
@@ -1,68 +1,67 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import {Input, Select, Avatar, Menu, Dropdown} from 'antd'
-import {Link} from 'react-router-dom'
+import { Layout, Row, Col, Input, Select, Dropdown, Avatar, Menu } from 'antd';
+import { Link } from 'react-router-dom'
 import './AppHeader.scss'
 
-const {Search} = Input
-const {Option} = Select
+const { Header, Content } = Layout
+const { Search } = Input;
+const { Option } = Select
 
 const AppHeader = () => {
-    const menu = (
-        <Menu>
-          <Menu.Item key="0">
-            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-              Xem hô sơ
+  const menu = (
+    <Menu>
+      <Menu.Item key="0">
+        <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+          Xem hô sơ
             </a>
-          </Menu.Item>
-          <Menu.Item key="1">
-            <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
-              Cài đặt
+      </Menu.Item>
+      <Menu.Item key="1">
+        <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+          Cài đặt
             </a>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="3">
+        Đổi mật khẩu
           </Menu.Item>
-          <Menu.Divider />
-          <Menu.Item key="3">
-            Đổi mật khẩu
+      <Menu.Item key="4">
+        Đăng xuất
           </Menu.Item>
-          <Menu.Item key="4">
-            Đăng xuất
-          </Menu.Item>
-        </Menu>
-      );
+    </Menu>
+  );
 
-    return (
-            <Row className="header">
-                <Col xs={2} className="flex-center">
-                  <Link to="/">
-                    <img src="./asset/logo.png" style={{maxWidth:"100%", maxHeight:"100%"}}></img>
-                  </Link>
-                    
-                </Col>
-                <Col xs={8} className="flex-center">
-                    <Select defaultValue="book" showArrow={false}>
-                        <Option value="book">Sách</Option>
-                        <Option value="author">Tác giả</Option>
-                        <Option value="user">Người dùng</Option>
-                    </Select>
-                    <Search
-                        placeholder="Tìm kiếm"
-                        onSearch={value => console.log(value)}
-                        style={{ width: '50%', borderRadius:"0" }}
-                        size="small"
-                    />
-                </Col>
-                <Col xs={2}  className="flex-end">
-                  <Dropdown overlay={menu} placement="bottomLeft">     
-                  <div>
-                      <Avatar style={{ backgroundColor: "orange", verticalAlign: 'middle' }} size="small">
-                          N
-                      </Avatar>
-                      <strong style={{marginLeft:"10px", color:"#ffffffc4"}}>Ngoc Trinh</strong>
-                  </div>     
-                  </Dropdown>   
-                </Col>     
-            </Row>
-    )
+  return (
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%', background: "#8900d4" }}>
+      <Row justify="space-between" align="middle" type="flex">
+        <Col>
+          <img src="./asset/logo.png" style={{ height: 64 }}></img>
+        </Col>
+        <Col xs={8} className="flex-center">
+          <Select id="search-select" defaultValue="book" showArrow={false} getPopupContainer={() => document.getElementById("search-select")}>
+            <Option value="book">Sách</Option>
+            <Option value="author">Tác giả</Option>
+            <Option value="user">Người dùng</Option>
+          </Select>
+          <Search
+            placeholder="Tìm kiếm"
+            onSearch={value => console.log(value)}
+            style={{ width: '80%', borderRadius: "0" }}
+            size="small"
+          />
+        </Col>
+        <Col>
+          <Dropdown overlay={menu} placement="bottomLeft" getPopupContainer={() => document.getElementById("user-dropdown")}>
+            <div id="user-dropdown" style={{height: 30, display: "flex", alignItems: "center"}}>
+              <Avatar style={{ backgroundColor: "orange", verticalAlign: 'middle' }} size="small">
+                N
+              </Avatar>
+              <strong style={{ marginLeft: "10px", color: "#ffffffc4" }}>Ngoc Trinh</strong>
+            </div>
+          </Dropdown>
+        </Col>
+      </Row>
+    </Header>
+  )
 }
 
 export default AppHeader
